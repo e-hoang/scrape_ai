9bef5ec1ed410d8a7fdfca9c0f32079d876e0198
e-hoang <erictuanhoang@gmail.com>
2025-06-23 17:27:22 -0600

    python web scraper

--- scrape.py
+++ scrape.py
@@ -1,7 +1,18 @@
 from bs4 import BeautifulSoup
+import requests
 
 def main():
-    print()  
+    url = 'https://finance.yahoo.com/markets/'
+    headers = {'User-Agent' : 'Mozilla/5.0'}
+    page = requests.get(url, headers=headers) 
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    ticker_spans = soup.findAll('span', class_='symbol yf-1jsynna')
+    
+    for ticker in ticker_spans:
+        symbol = ticker.get_text(strip=True)
+        if symbol:
+            print(symbol)
 
 if __name__ == '__main__':
     main()
